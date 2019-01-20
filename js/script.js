e287c7680c42bdb75ae165c94c513bb88a453c5c
valery-2018 <44970776+valery-2018@users.noreply.github.com>
2019-01-20 17:54:24 +0300

    project Technomart completion(2)

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@ isStorageSupport = false;
 }
 
 link.addEventListener("click", function (evt) {evt.preventDefault();
-  popup.classList.add("modal-show");
+popup.classList.add("modal-show");
   if (storage) {
-      myname.value = storage;
-      email.focus();
+    myname.value = storage;
+    email.focus();
   } else {
-   myname.focus();
+    myname.focus();
 }
 });
 close.addEventListener("click", function (evt) {
@@ -30,16 +30,16 @@ popup.classList.remove("modal-show");
 popup.classList.remove("modal-error");
 });
 form.addEventListener("submit", function (evt) {
-  if (!myname.value || !email.value) {
-    evt.preventDefault();
-    popup.classList.remove("modal-error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
+if (!myname.value || !email.value) {
+  evt.preventDefault();
+  popup.classList.remove("modal-error");
+  popup.offsetWidth = popup.offsetWidth;
+  popup.classList.add("modal-error");
   } else {
-    if (isStorageSupport) {
-      localStorage.setItem("myname", myname.value);
-    }
+  if (isStorageSupport) {
+    localStorage.setItem("myname", myname.value);
   }
+}
 });
 window.addEventListener("keydown", function (evt) {
 if (evt.keyCode === 27) {
